f185dac7a01702ca0b6b371b035e6cb670287dd1
Jim Hefferon <jhefferon@smcvt.edu>
2016-03-04 17:22:40 -0500

    rooms part of script

--- bin/hrumc.py
+++ bin/hrumc.py
@@ -11,6 +11,8 @@ import sys, os, os.path, re, pprint, argparse, traceback, time
 import tempfile, shutil, glob
 import subprocess
 
+DEFAULT_PROGRAM_NAME = "hrumc2016.tex"  # name of the conference program
+
 # Global variables spare me from putting them in the call of each fcn.
 VERBOSE = False
 DEBUG = False
@@ -48,12 +50,13 @@ LATEX_TEMPLATE = r"""\documentclass[12pt]{article}
 %% #1 title 
 %% #2 author 
 %% #3 level 
-%% #4 abstract body
-\renewcommand{\abstract}[4]{%% \newcommand is \long by default
+%% #4 subject
+%% #5 abstract body
+\renewcommand{\abstract}[5]{%% \newcommand is \long by default
    \begin{center} \abstracttitle{#1} \end{center}
    \begin{center} \abstractspeaker{#2} \end{center} 
    \begin{center} \abstractlevel{#3} \end{center} 
-   \noindent #4 
+   \noindent #5
 } 
 \pagestyle{empty}
 \begin{document}\thispagestyle{empty} 
@@ -79,19 +82,20 @@ LATEX_ALL_TEMPLATE_TOP = r"""\documentclass[11pt]{article}
 %% #1 title 
 %% #2 author 
 %% #3 level 
-%% #4 abstract body
-\renewcommand{\abstract}[4]{%% \newcommand is \long by default
+%% #4 subject 
+%% #5 abstract body
+\renewcommand{\abstract}[5]{%% \newcommand is \long by default
    \abstracttitle{#1} \\
    \abstractlevel{#3}  
    \abstractspeaker{#2} \\
-   \noindent #4 
+   \noindent #5 
 } 
 \pagestyle{plain}
 \begin{document}\RaggedRight
 """
 
 
-LATEX_ROOMS_TEMPLATE_TOP = r"""\documentclass[11pt]{article}
+LATEX_ROOMS_TEMPLATE_TOP = r"""\documentclass[12pt]{article}
 \usepackage{cmap}
 \usepackage[utf8]{inputenc}
 \usepackage{amsmath,amssymb} 
@@ -100,21 +104,54 @@ LATEX_ROOMS_TEMPLATE_TOP = r"""\documentclass[11pt]{article}
 \usepackage[top=1in,bottom=1in,right=1in,left=1in]{geometry}
 
 \usepackage{ragged2e} %% for RaggedRight
+\usepackage{enumitem} %% for description
+
+\newcommand{\sessionhead}[1]{\begin{center}\Large \textbf{Session #1}\end{center}}
+\newcommand{\session}[3]{\begin{center}{\large \textbf{#1}} \\ Chair: #3\end{center}}
+\newcommand{\at}[2]{%
+  \begin{description}[font=\normalfont,leftmargin=2em,labelwidth=0em,topsep=0ex plus 1pt] 
+    \RaggedRight
+    \item[\abstracttime{#1}]
+    \input{#2}
+  \end{description}
+}
+\newcommand{\abstracttime}[1]{#1}
+% Times
+\newcommand{\timeformat}[2]{#1:#2}
+\newcommand{\Ia}{\timeformat{10}{00}-\timeformat{10}{15}}
+\newcommand{\Ib}{\timeformat{10}{20}-\timeformat{10}{35}}
+\newcommand{\Ic}{\timeformat{10}{40}-\timeformat{10}{55}}
+\newcommand{\IIa}{\timeformat{1}{40}-\timeformat{1}{55}}
+\newcommand{\IIb}{\timeformat{2}{00}-\timeformat{2}{15}}
+\newcommand{\IIc}{\timeformat{2}{20}-\timeformat{2}{35}}
+\newcommand{\IIIa}{\timeformat{3}{30}-\timeformat{3}{45}}
+\newcommand{\IIIb}{\timeformat{4}{00}-\timeformat{4}{15}}
+\newcommand{\IIIc}{\timeformat{4}{20}-\timeformat{4}{35}}
+
 \newcommand{\abstracttitle}[1]{\textbf{#1}}
 \newcommand{\abstractlevel}[1]{\textrm{(Level~#1)}}
 \newcommand{\abstractspeaker}[1]{\textsc{#1}}
+\newcommand{\affiliation}[1]{ (#1)}
 
 %% Abstract 
 %% #1 title 
 %% #2 author 
 %% #3 level 
-%% #4 abstract body
-\renewcommand{\abstract}[4]{%% \newcommand is \long by default
+%% #4 subject 
+%% #5 abstract body
+\renewcommand{\abstract}[5]{%% \newcommand is \long by default
    \abstracttitle{#1} 
    \abstractspeaker{#2} 
    \abstractlevel{#3}  
-   %% \noindent #4 
 } 
+
+\newenvironment{room}[1]{%
+  \begin{center}\Huge #1\end{center}
+  \bigskip
+}{%
+  \clearpage
+}
+
 \pagestyle{empty}
 \begin{document}\RaggedRight
 """
@@ -192,7 +229,7 @@ def make_rooms(inputfn, filelist, outputfn="rooms"):
                 else:
                     raise HRUMCException('Expected a match for line number '+str(linenumber)+', the session line '+line)
             elif line.startswith("\\at{"):
-                print("line starts with at")
+                # print("line starts with at")
                 m = at_re.match(line)
                 if m:
                     attime, atabstract = m.group(1), m.group(2)
@@ -212,10 +249,10 @@ def make_rooms(inputfn, filelist, outputfn="rooms"):
     fout = open(outputfn+'.tex','w')
     print(LATEX_ROOMS_TEMPLATE_TOP, file=fout)
     for k in rooms:
-        print("\begin{room}{xx}", file=fout)
+        print(r"\begin{room}{%s}" % (k,), file=fout)
         print("\n".join(rooms[k]), file=fout)
-        print("\end{room}", file=fout)
-    print("\end{document}", file=fout)
+        print(r"\end{room}", file=fout)
+    print(r"\end{document}", file=fout)
     fout.close()
     # shutil.copyfile('./'+jobname+'-crop.pdf', pdfdirname+'/'+jobname+'.pdf')
     # clean up
@@ -297,18 +334,19 @@ def main (args):
     os.mkdir(OUTPUT_DIR_NAME)
     # get all .tex files in this dir
     filelist = glob.glob("*.tex")
-    filelist.remove("hrumc2016.tex")
+    filelist.remove(args['file'])
     filelist.sort()
     # for fn in filelist:
     #     latex_each(fn, pdfdirname=OUTPUT_DIR_NAME)
     # latex_all('hrumcall',filelist)
-    make_rooms("hrumc2016.tex",filelist)
+    make_rooms(args['file'],filelist)
 
 #==================================================================
 if __name__ == '__main__':
     try:
         start_time = time.time()
         parser = argparse.ArgumentParser(description=globals()['__doc__'])
+        parser.add_argument('-f','--file', action='store', default=DEFAULT_PROGRAM_NAME,help='name of the .tex of the program for the conference')
         parser.add_argument('-v','--version', action='version', version='%(prog)s '+globals()['__version__'])
         parser.add_argument('-D', '--debug', action='store_true', default=False, help='run debugging code')
         parser.add_argument('-V', '--verbose', action='store_true', default=False, help='verbose output')
